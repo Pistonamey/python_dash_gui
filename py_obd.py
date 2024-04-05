456c9a1d2b72168c05d1593085723fd15a47a9bc
luc18pham <113390946+luc18pham@users.noreply.github.com>
2024-04-05 12:36:14 -0500

    Add files via upload

--- py_obd.py
+++ py_obd.py
@@ -4,7 +4,14 @@ connection = obd.OBD() # auto-connects to USB or RF port
 
 cmd = obd.commands.SPEED # select an OBD command (sensor)
 
-response = connection.query(cmd) # send the command, and parse the response
+response = connection.query(cmd, force=True) # send the command, and parse the response
+
+print(connection.status())
 
 print(response.value) # returns unit-bearing values thanks to Pint
 print(response.value.to("mph")) # user-friendly unit conversions
+
+
+ports = obd.scan_serial()      # return list of valid USB or RF ports
+print(ports)                    # ['/dev/ttyUSB0', '/dev/ttyUSB1']
+connection = obd.OBD(ports[0]) # connect to the first port in the list
